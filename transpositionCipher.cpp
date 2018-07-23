7a1fc1f79e29cffd420bc52c4b2676cb54100ceb
vigilanti <hasheersheikh@gmail.com>
2018-07-23 14:11:39 -0400

    actually encypryted

--- transpositionCipher.cpp
+++ transpositionCipher.cpp
@@ -3,23 +3,47 @@ using namespace std;
 
 //program for transposition cipher
 
-int main (){
-        string s;
-        cout<<"Enter the string -->  ";
-        cin>>s;
-        for(int i=0,n=0;i<s.length();i++){
-                if((n++)%2==0){
-                        
-                
-                }
-                int val=s[i];
-                val=val+3;
-                s[i]=char(val);
-                
-                
-                                
+//function for encryption with transposition
+class TranspositionEncryption {
+        //code for encrypting the string
+        public: 
+        string encrypt(string s){
+                string ency=s;
+                int mid=s.length()/2;
+                for(int i=0,j=0;i<s.length();i++){
+                        if(i%2==0)
+                            ency[j++]=s[i];
+                        if(i%2==1)
+                            ency[mid++]=s[i];
+                }       
+                return ency;
         }
         
-        cout<<"String is --> "<<s;
+        //code for decrypting the cipher
+        string decryption(string s){
+                string decrypt=s;
+                int mid=s.length()/2;
+                for(int i=0,j=0;i<s.length();i++){
+                        if(i%2==0)
+                            decrypt[i]=s[j++];
+                        if(i%2==1)
+                            decrypt[i]=s[mid++];
+                }       
+                return decrypt;
+        }
+};
+
+//main function
+
+int main (){
+        string s,ency,decy;
+        cout<<"Enter the string -->  ";
+        cin>>s;      	                                 //scanning the string  
+        TranspositionEncryption e;
+        ency=e.encrypt(s);
+        decy=e.decryption(ency);
+        cout<<"String is --> "<<ency<<endl;
+        cout<<"String is --> "<<decy<<endl;
         return 0;
 }
+
